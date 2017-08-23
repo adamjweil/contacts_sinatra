9c5d9f1b783a51f8959c1c6634a7a76551fe6d6b
Adam Weil <adamjweil@gmail.com>
2017-08-23 14:00:19 -0400

    added create contact functionality

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,43 @@
 get '/' do
+  redirect '/contacts'
+end
+
+# index
+get '/contacts' do
   @contacts = Contact.all
-  erb :index
+  erb :'contacts/index'
+end
+
+
+# new
+get '/contacts/new' do
+  erb :'contacts/new'
+end
+
+# create
+post '/contacts' do
+  puts params
+  @contact = Contact.create(params[:contact])
+  redirect '/contacts'
+end
+
+# show
+get '/contacts/:id' do
+  @contact = Contact.find(params[:id])
+  erb :'contacts/show'
+end
+
+# edit
+get '/contacts/:id/edit' do
+  @contact = Contact.find(params[:id])
+  erb :'edit'
+end
+
+# update
+patch '/contacts/:id' do
+
+end
+
+put '/contacts/:id' do
+
 end
